203973d66c9468bdff852d66e86da55c21e0767c
Virgil Dupras <hsoft@hardcoded.net>
2010-04-07 08:52:24 +0200

    Licensed under BSD.

--- modules/send2trash_osx.c
+++ modules/send2trash_osx.c
@@ -1,3 +1,10 @@
+/* Copyright 2010 Hardcoded Software (http://www.hardcoded.net)
+
+This software is licensed under the "BSD" License as described in the "LICENSE" file, 
+which should be included with this package. The terms are also available at 
+http://www.hardcoded.net/licenses/bsd_license
+*/
+
 #define PY_SSIZE_T_CLEAN
 #include "Python.h"
 #include <CoreServices/CoreServices.h>
