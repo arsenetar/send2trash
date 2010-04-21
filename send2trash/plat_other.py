f5f9c5b35250405f54bb67fc48e5c4ec8934d7cf
Virgil Dupras <hsoft@hardcoded.net>
2010-04-21 10:30:51 +0200

    Fixed the copyright comment which was at a strange place.

--- send2trash/plat_other.py
+++ send2trash/plat_other.py
@@ -1,13 +1,13 @@
-from __future__ import unicode_literals
-import sys
-import os
-import os.path as op
 # Copyright 2010 Hardcoded Software (http://www.hardcoded.net)
 
 # This software is licensed under the "BSD" License as described in the "LICENSE" file, 
 # which should be included with this package. The terms are also available at 
 # http://www.hardcoded.net/licenses/bsd_license
 
+from __future__ import unicode_literals
+import sys
+import os
+import os.path as op
 import logging
 
 CANDIDATES = [
