a324923ffabdf63fbb3d6fedc1e4720c4230ab61
Andrew Senetar <arsenetar@gmail.com>
2021-03-02 19:23:43 -0600

    Add IFileOperationProgressSink
    - Add sink to allow detection of times when the file would be deleted
    - Currently can detect, but not stop operations, more work needed

--- send2trash/plat_win_modern.py
+++ send2trash/plat_win_modern.py
@@ -11,6 +11,7 @@ from platform import version
 import pythoncom
 import pywintypes
 from win32com.shell import shell, shellcon
+from .IFileOperationProgressSink import CreateSink
 
 
 def send2trash(paths):
@@ -50,10 +51,11 @@ def send2trash(paths):
     # actually try to perform the operation, this section may throw a
     # pywintypes.com_error which does not seem to create as nice of an
     # error as OSError so wrapping with try to convert
+    sink = CreateSink()
     try:
         for path in paths:
             item = shell.SHCreateItemFromParsingName(path, None, shell.IID_IShellItem)
-            fileop.DeleteItem(item)
+            fileop.DeleteItem(item, sink)
         result = fileop.PerformOperations()
         aborted = fileop.GetAnyOperationsAborted()
         # if non-zero result or aborted throw an exception
